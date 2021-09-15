a3aa7ba88d3b3a392d01ada5c68615e891aa42bd
Edward Mello <edwardm@za.neolife.com>
2021-09-15 15:04:34 +0200

    encryption hash using salt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 //const encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -32,13 +33,19 @@ app.get("/login",(req,res)=>{
 });
 app.post("/login",(req,res)=>{
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({email:username},(err,foundUser)=>{
     if(err){
       console.log(err);
     }else {
-      if (foundUser.password === password) {
-        res.render('secrets');
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            // result == true
+            if(result === true){
+              res.render('secrets');
+            }
+        });
+
       }
     }
   })
@@ -50,17 +57,22 @@ app.get("/register",(req,res)=>{
   res.render('register');
 });
 app.post("/register",(req,res)=>{
-  const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+          email:req.body.username,
+          password:hash
+      });
+      newUser.save((err)=>{
+        if(err){
+          console.log(err);
+        }else {
+          res.render('secrets');
+        }
+      });
     });
-  newUser.save((err)=>{
-    if(err){
-      console.log(err);
-    }else {
-      res.render('secrets');
-    }
-  });
+
+
 });
 
 app.listen(3000,()=>{

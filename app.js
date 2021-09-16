4734b1e4ac0fcacf6238fb241d4089056771219a
Edward Mello <edwardm@za.neolife.com>
2021-09-16 09:35:46 +0200

    authentication using passport

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 //const encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -14,6 +16,16 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(session({
+  secret:"lesibamalelagutoma",
+  resave:false,
+  saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema({
@@ -21,8 +33,15 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User",userSchema);
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",(req,res)=>{
   res.render('home');
@@ -32,48 +51,50 @@ app.get("/login",(req,res)=>{
   res.render('login');
 });
 app.post("/login",(req,res)=>{
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email:username},(err,foundUser)=>{
-    if(err){
+  const user = new User ({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user,(err)=>{
+    if (err) {
       console.log(err);
-    }else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-            // result == true
-            if(result === true){
-              res.render('secrets');
-            }
-        });
-
-      }
+    } else {
+      passport.authenticate("local")(req,res, ()=>{
+        res.redirect('/secrets');
+      })
     }
-  })
+  });
 
+});
 
+app.get("/secrets",(req,res)=>{
+  if (req.isAuthenticated()) {
+    res.render('secrets');
+  } else {
+    res.redirect("/login")
+  }
 });
 
 app.get("/register",(req,res)=>{
   res.render('register');
 });
 app.post("/register",(req,res)=>{
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-          email:req.body.username,
-          password:hash
-      });
-      newUser.save((err)=>{
-        if(err){
-          console.log(err);
-        }else {
-          res.render('secrets');
-        }
-      });
-    });
-
+  User.register({username: req.body.username}, req.body.password ,(err,user)=>{
+    if (err) {
+      res.redirect('register');
+    } else {
+      passport.authenticate("local")(req,res, ()=>{
+        res.redirect('/secrets');
+      })
+    }
+  });
 
 });
+
+app.get("/logout",(req,res)=>{
+  req.logout();
+  res.redirect("/")
+})
 
 app.listen(3000,()=>{
   console.log('Surver started on port 3000');
